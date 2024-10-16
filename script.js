f66f635bc77416340bfeea60f3e30623fc9f40c7
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-16 08:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toCase(text) {
   // write your code here
+	let t1=text.toUppercase();
+	let ans=text+"-"+t1
+	return ans;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
